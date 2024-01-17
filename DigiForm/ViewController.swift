6bc2fdf0befa43b941eab9ea7c5fa706ace6b060
Daniel Sanchez <dst990502@outlook.com>
2024-01-16 19:15:03 -0600

    Initial Commit

---
+++ DigiForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigiForm
+//
+//  Created by Iván Sánchez Torres on 16/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
