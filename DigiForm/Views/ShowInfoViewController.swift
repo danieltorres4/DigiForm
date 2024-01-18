a2a834cab44bc571438007142a54ba5c47e74e2d
Daniel Sanchez <dst990502@outlook.com>
2024-01-18 00:22:31 -0600

    ShowInfoVC was added

---
+++ DigiForm/Views/ShowInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShowInfoViewController.swift
+//  DigiForm
+//
+//  Created by Daniel ST on 18/01/24.
+//
+
+import UIKit
+
+class ShowInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
