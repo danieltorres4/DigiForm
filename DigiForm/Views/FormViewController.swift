a4c04f64334d76f81866833920dec011d5f3e3ab
Daniel Sanchez <dst990502@outlook.com>
2024-01-17 20:35:22 -0600

    Subviews were added. New VC was created for the form

---
+++ DigiForm/Views/FormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FormViewController.swift
+//  DigiForm
+//
+//  Created by Daniel ST on 17/01/24.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ DigiForm/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  DigiForm
+//
+//  Created by Daniel ST on 16/01/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var fillFormBtn: UIButton!
+    @IBOutlet weak var showInfoBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
