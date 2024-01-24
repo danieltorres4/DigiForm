f55bbeaa87ce2982695c6b40dca68eb52f31b747
Daniel Sanchez <dst990502@outlook.com>
2024-01-23 19:20:11 -0600

    [UPDATE] main view controller reference has been deleted
    Subviews were added programmatically

--- DigiForm/Views/MainViewController.swift
+++ DigiForm/Views/MainViewController.swift
@@ -9,10 +9,51 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var fillFormBtn: UIButton!
-    @IBOutlet weak var showInfoBtn: UIButton!
-    @IBOutlet weak var mainImageView: UIImageView!
-    @IBOutlet weak var digiFormTitleLabel: UILabel!
+    private let mainIV: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "eou2")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let mainLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = "DigiForm"
+        label.font = UIFont(name: "Rockwell", size: 50)
+        label.textColor = UIColor(red: 28/255.0, green: 103/255.0, blue: 41/255.0, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var fillFormButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Fill Form"
+        config.background.backgroundColor = UIColor(red: 28/255.0, green: 103/255.0, blue: 41/255.0, alpha: 1.0)
+        
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(fillFormVC), for: .touchUpInside)
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var showSavedDataButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Show Info"
+        config.background.backgroundColor = UIColor(red: 28/255.0, green: 103/255.0, blue: 41/255.0, alpha: 1.0)
+        config.titleAlignment = .center
+        
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(showSavedDataVC), for: .touchUpInside)
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +65,41 @@ class MainViewController: UIViewController {
         gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
         gradientLayer.frame = view.bounds
         view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        // SETTING UP SUBVIEWS
+        [mainLabel, mainIV, fillFormButton, showSavedDataButton].forEach(view.addSubview)
+        
+        // CONSTRAINTS
+        var gridValue = (view.bounds.width) / 5 // ancho pantalla / 5
+        NSLayoutConstraint.activate([
+            mainLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
+            mainLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            // IV
+            mainIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainIV.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 50),
+            mainIV.widthAnchor.constraint(equalToConstant: 300),
+            mainIV.heightAnchor.constraint(equalToConstant: 300),
+            
+            // BUTTONS
+            fillFormButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: gridValue),
+            fillFormButton.topAnchor.constraint(equalTo: mainIV.bottomAnchor, constant: 20),
+            showSavedDataButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -gridValue),
+            showSavedDataButton.topAnchor.constraint(equalTo: mainIV.bottomAnchor, constant: 20),
+            showSavedDataButton.widthAnchor.constraint(equalTo: fillFormButton.widthAnchor),
+            fillFormButton.trailingAnchor.constraint(equalTo: showSavedDataButton.leadingAnchor, constant: -gridValue)
+        ])
+    }
+}
+
+
+extension MainViewController {
+    @objc func fillFormVC() {
+        
+    }
+    
+    @objc func showSavedDataVC() {
+        
     }
 }
